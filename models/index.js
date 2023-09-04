613345bde1441eb2d37ff2d8a41822c409f3eba9
Muhammad Waseem Javed <waseemjaved755@gmail.com>
2023-09-04 22:59:44 +0500

    added all schemas

--- models/index.js
+++ models/index.js
@@ -29,7 +29,7 @@ Play.belongsTo(User, {
   foreignKey: { name: "userId", allowNull: false },
 });
 
-//Junction Table [PlaylistVideos]
+//Junction Table.1 [PlaylistVideos]
 // 1:M Playlist <--> PlaylistVideos
 // 1:M Video <--> PlayListVideos
  //Created PlayListVideos and using PlaylistID and VideoID there (M:M)
@@ -43,6 +43,48 @@ PlaylistVideos.belongsTo(Video, {
   foreignKey: { name: "videoId", allowNull: false },
 });
 
+//Junction Table.2 [History]
+// 1:M User <--> History
+// 1:M Video <-->History
+ //Created History and using userID and VideoID there (M:M)
+
+ User.hasMany(History, { foreignKey: "cartId" });
+History.belongsTo(User, {
+  foreignKey: { name: "playlistId", allowNull: false },
+});
+Video.hasMany(History, { foreignKey: "productId" });
+History.belongsTo(Video, {
+  foreignKey: { name: "videoId", allowNull: false },
+});
+
+//Junction Table.3 [LikeVideos]
+// 1:M User <--> LikeVideos
+// 1:M Video <--> LikeVideos
+ //Created LikeVideos and using userID and VideoID there (M:M)
+
+ User.hasMany(LikeVideos, { foreignKey: "cartId" });
+ LikeVideos.belongsTo(User, {
+  foreignKey: { name: "playlistId", allowNull: false },
+});
+Video.hasMany(LikeVideos, { foreignKey: "productId" });
+LikeVideos.belongsTo(Video, {
+  foreignKey: { name: "videoId", allowNull: false },
+});
+
+
+//Junction Table.4 [WatchVideos]
+// 1:M User <--> WatchVideos
+// 1:M Video <-->WacthVideos
+ //Created WatchVideos and using userID and VideoID there (M:M)
+
+ User.hasMany(WatchVideo, { foreignKey: "cartId" });
+WatchVideo.belongsTo(User, {
+  foreignKey: { name: "playlistId", allowNull: false },
+});
+Video.hasMany(WatchVideo, { foreignKey: "productId" });
+WatchVideo.belongsTo(Video, {
+  foreignKey: { name: "videoId", allowNull: false },
+});
 
 
 
